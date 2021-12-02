37a450e36f748aca03163edc0e2e77006819acfa
Lilian Juri <lilianjuri17@gmail.com>
2021-12-01 22:46:14 -0300

    css changes in menu

--- src/Menu.js
+++ src/Menu.js
@@ -1,22 +1,24 @@
-import React from 'react'
+import React from 'react';
+import './App.css';
 
 const Menu = ({items}) => {
-    return <div>
-        {items.map((menuItem)=>{
-            const {id,nombre,img,desc,precio} = menuItem;
-            return <article key={id} className="menu-item">
-                <img src={img} alt={nombre} className='foto'/>
-                <div className='item-info'>
-                    <header>
-                        <h4>{nombre}</h4>
-                        <h4 className="precio">{precio}</h4>
-                    </header>
-                    <p className='item-text'>{desc}</p>
+    return <div className="container-menu">
+                <div className="menu">
+                    {items.map((menuItem)=>{
+                        const {id,nombre,img,desc,precio} = menuItem;
+                        return <article key={id} className="menu-item">
+                                    <img src={img} alt={nombre} className='foto'/>
+                                    <div className='item-info'>
+                                        <header>
+                                            <h4>{nombre}</h4>
+                                            <h4 className="precio">{precio}</h4>
+                                        </header>
+                                        <p className='item-text'>{desc}</p>
+                                    </div>
+                                </article>
+                    })}
                 </div>
-            </article>
-        })}
-
-    </div>
+            </div>
 }
 
 export default Menu;
--- src/datos.js
+++ src/datos.js
@@ -1,186 +1,122 @@
 const menu = [
     {
         id: 1,
-        nombre: 'torta de chocolate',
-        categoria: 'tortas comunes',
+        nombre: 'Torta de Chocolate',
+        categoria: 'Tortas Comunes',
         precio: 100,
-        img: './imagen/producto-1',
+        img: './src/imagen/producto-1.JPG',
         desc: 'Es en bizcochuelo sabor chocolate'
     },
     {
         id: 2,
-        nombre: 'torta de vainilla',
-        categoria: 'tortas comunes',
+        nombre: 'Torta de Vainilla',
+        categoria: 'Tortas Comunes',
         precio: 200,
-        img: './imagen/producto-2',
+        img: './src/imagen/producto-2.JPG',
         desc: 'Es un bizcochuelo sabor vainilla'
     },
     {
         id: 3,
-        nombre: 'torta de dulce de leche',
-        categoria: 'tortas comunes',
+        nombre: 'Torta de Dulce de Leche',
+        categoria: 'Tortas Comunes',
         precio: 200,
-        img: './imagen/producto-3',
+        img: './src/imagen/producto-3.JPG',
         desc: 'Es un bizcochuelo sabor dulce de leche'
     },
     {
         id: 4,
-        nombre: 'torta de frutilla',
-        categoria: 'tortas comunes',
+        nombre: 'Torta de Frutilla',
+        categoria: 'Tortas Comunes',
         precio: 200,
         img: './imagen/producto-4',
         desc: 'Es un bizcochuelo sabor frutilla'
     },
     {
         id: 5,
-        nombre: 'torta frutos rojos',
-        categoria: 'tortas especiales',
+        nombre: 'Torta Frutos Rojos',
+        categoria: 'Tortas Especiales',
         precio: 400,
         img: './imagen/producto-5',
         desc: 'Es una torta con base de bizcochuelo de vainila o chocolate con crema y frutos rojos.'
     },
     {
         id: 6,
-        nombre: 'torta cumpleaños',
-        categoria: 'tortas especiales',
+        nombre: 'Torta Cumpleaños',
+        categoria: 'Tortas Especiales',
         precio: 600,
         img: './imagen/producto-6',
         desc: 'Es una torta con base de bizcochuelo comun y decorada a pedido con el motivo seleccionado por el cliente.'
     },
     {
         id: 7,
-        nombre: 'lemon pie',
-        categoria: 'tortas especiales',
+        nombre: 'Lemon Pie',
+        categoria: 'Tortas Especiales',
         precio: 400,
         img: './imagen/producto-7',
         desc: 'Es una tarta con crema de limon y decorada con merengue flameado.'
     },
     {
         id: 8,
-        nombre: 'selva negra',
-        categoria: 'tortas especiales',
+        nombre: 'Selva Negra',
+        categoria: 'Tortas Especiales',
         precio: 400,
         img: './imagen/producto-8',
         desc: 'Es una torta con base de bizcochuelo de chocolate rellena de crema y decorada con detalles de chocolate'
     },
     {
         id: 9,
-        nombre: 'torta rogel',
-        categoria: 'tortas especiales',
+        nombre: 'Torta Rogel',
+        categoria: 'Tortas Especiales',
         precio: 400,
         img: './imagen/producto-9',
         desc: 'Es una torta compuesta de laminas intercaladas con dulce de leche y decorada con merengue flameado'
     },
     {
         id: 10,
-        nombre: 'tiramisu',
-        categoria: 'postres',
+        nombre: 'Tiramisu',
+        categoria: 'Postres',
         precio: 300,
         img: './imagen/producto-10',
         desc: 'Postre en realizado con crema y mascarpone con vainillas y cubierto de cacao de chocolate'
     },
     {
         id: 11,
-        nombre: 'vc',
-        categoria: 'fv',
+        nombre: 'Flan',
+        categoria: 'Postres',
         precio: 200,
         img: './imagen/producto-11',
         desc: 'fb'
     },
     {
         id: 12,
-        nombre: 'vc',
-        categoria: 'fv',
+        nombre: 'Mousse de Limon',
+        categoria: 'Postres',
         precio: 200,
         img: './imagen/producto-12',
         desc: 'fb'
     },
     {
-        id: 13,
-        nombre: 'vc',
-        categoria: 'fv',
-        precio: 200,
-        img: './imagen/producto-13',
-        desc: 'fb'
-    },
-    {
-        id: 14,
-        nombre: 'vc',
-        categoria: 'fv',
-        precio: 200,
-        img: './imagen/producto-14',
-        desc: 'fb'
-    },
-    {
         id: 15,
-        nombre: 'vc',
-        categoria: 'fv',
+        nombre: 'Budin de Pan',
+        categoria: 'Postres',
         precio: 200,
         img: './imagen/producto-15',
         desc: 'fb'
     },
     {
-        id: 16,
-        nombre: 'vc',
-        categoria: 'fv',
-        precio: 200,
-        img: './imagen/producto-16',
-        desc: 'fb'
-    },
-    {
         id: 17,
-        nombre: 'vc',
-        categoria: 'fv',
+        nombre: 'Choco Torta',
+        categoria: 'Tortas Especiales',
         precio: 200,
         img: './imagen/producto-17',
         desc: 'fb'
     },
     {
         id: 18,
-        nombre: 'vc',
-        categoria: 'fv',
+        nombre: 'Cheseecake',
+        categoria: 'Postres',
         precio: 200,
         img: './imagen/producto-18',
-        desc: 'fb'
-    },
-    {
-        id: 19,
-        nombre: 'vc',
-        categoria: 'fv',
-        precio: 200,
-        img: './imagen/producto-19',
-        desc: 'fb'
-    },
-    {
-        id: 20,
-        nombre: 'vc',
-        categoria: 'fv',
-        precio: 200,
-        img: './imagen/producto-20',
-        desc: 'fb'
-    },
-    {
-        id: 21,
-        nombre: 'vc',
-        categoria: 'fv',
-        precio: 200,
-        img: './imagen/producto-21',
-        desc: 'fb'
-    },
-    {
-        id: 22,
-        nombre: 'vc',
-        categoria: 'fv',
-        precio: 200,
-        img: './imagen/producto-22',
-        desc: 'fb'
-    },
-    {
-        id: 23,
-        nombre: 'vc',
-        categoria: 'fv',
-        precio: 200,
-        img: './imagen/producto-23',
         desc: 'fb'
     }]
     
